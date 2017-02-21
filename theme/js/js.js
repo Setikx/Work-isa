532f1d45e84e20fae8262aae62001f1290bbb1f4
Setikx <seda.wow@gmail.com>
2017-02-21 18:26:43 +0300

    working directory uploaded

---
+++ theme/js/js.js
@@ -0,0 +1,10 @@
+
+$(document).ready()
+$(window).load(function() {
+    $('.flexslider').flexslider({
+        animation: "slide",
+        prevText: "",           //String: Set the text for the "previous" directionNav item
+        nextText: "",
+        slideshowSpeed: 7000
+    });
+});
